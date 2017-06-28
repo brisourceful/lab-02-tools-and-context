24323cb9698cfeb6184891fa6c8cf82c529cf4c1
Brianna <briannaburrows@gmail.com>
2017-06-28 14:07:28 -0700

    added test for .call and .apply

--- lab-brianna/test/test.js
+++ lab-brianna/test/test.js
@@ -9,18 +9,32 @@ describe('parcel', function() {
     it('should return a Cookie object with two properties including White Chocolate and Large', function () {
       assert.equal(thing.type, 'White Chocolate');
       assert.equal(thing.size, 'Large');
-    })
+    });
+  });
+});
 
+describe('parcel', function() {
+  describe( '.call()', function() {
     let bucket = new parcel.MegaCookie('Snickerdoodle', 'Small');
     it('should return a MegaCookie object with three properties including Snickerdoodle, Small, True', function() {
       assert.equal(bucket.type, 'Snickerdoodle');
       assert.equal(bucket.size, 'Small');
     });
+    it('should return an object', function(){
+      assert.equal(typeof bucket, 'object');
+    });
+  });
+});
 
+describe('parcel', function () {
+  describe('.apply()', function () {
     let box = new parcel.DietCookie('Chocolate', 'Medium');
     it('should return an array', function() {
       assert.equal(box.type, 'Chocolate');
       assert.equal(box.size, 'Medium');
+    });
+    it('should return an object', function() {
+      assert.equal(typeof box, 'object');
     });
   });
 });
