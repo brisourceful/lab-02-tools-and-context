b28fc3bc26d452a8608e168ed6cdc7199f75ced0
Brianna <briannaburrows@gmail.com>
2017-06-28 13:23:32 -0700

    added test for Cookie constructor

--- lab-brianna/index.js
+++ lab-brianna/index.js
@@ -6,5 +6,5 @@ console.log(product);
 let packet = new product.MegaCookie('Chocolate Chip', 'Large');
 console.log(packet);
 
-let dietPacket = new product.DietCookie(['Skinny Bitch', 'Small'])
+let dietPacket = new product.DietCookie('Skinny Bitch', 'Small')
 console.log(dietPacket);
--- lab-brianna/test/test.js
+++ lab-brianna/test/test.js
@@ -5,11 +5,18 @@ const parcel = require('../lib/call-apply');
 
 describe('parcel', function() {
   describe('#call-apply.js', function() {
+    let thing = new parcel.Cookie('White Chocolate', 'Large')
+    it('should return a Cookie object with two properties including White Chocolate and Large', function () {
+      assert.equal(thing.type, 'White Chocolate');
+      assert.equal(thing.size, 'Large');
+    })
+
     let bucket = new parcel.MegaCookie('Snickerdoodle', 'Small');
     it('should return a MegaCookie object with three properties including Snickerdoodle, Small, True', function() {
       assert.equal(bucket.type, 'Snickerdoodle');
       assert.equal(bucket.size, 'Small');
     });
+
     let box = new parcel.DietCookie('Chocolate', 'Medium');
     it('should return an array', function() {
       assert.equal(box.type, 'Chocolate');
